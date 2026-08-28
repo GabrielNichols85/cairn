5deb7f9ae58b5b7f6f9be9f04a84ac2b3416708d
GabrielNichols85 <gabrielnicholsokc@gmail.com>
2026-08-28 13:36:53 +0200

    Cloudflare redirects and headers

--- build-preview.mjs
+++ build-preview.mjs
@@ -15,7 +15,7 @@ const MODULES = [
   '/src/circles.js', '/src/emails.js',
   '/src/views/today.js', '/src/views/wall.js', '/src/views/answered.js',
   '/src/views/journal.js', '/src/views/settings.js', '/src/views/circles.js',
-  '/src/views/unsubscribe.js', '/src/main.js',
+  '/src/views/unsubscribe.js', '/src/views/stats.js', '/src/main.js',
 ];
 
 const read = (p) => readFileSync('.' + p, 'utf8');
